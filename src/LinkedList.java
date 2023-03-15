4401e7091bdb2b18553f27f8e7b176eb80df13c8
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-15 22:29:04 +0530

    Added : Ability to insert 30 between 56 and 70

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,3 @@
-
 public class LinkedList<T> {
     public Node<T> head;
     public Node<T> tail;
@@ -24,8 +23,62 @@ public class LinkedList<T> {
             }
             System.out.println();
         }
-
     }
-
-
+    public void insert(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(tail);
+            tail = newNode;
+        }
+    }
+    public void append(T data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            tail = newNode;
+            head = newNode;
+        }else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+    public void popFirst() {
+        Node<T> temp = head.getNext();
+        head = temp;
+    }
+    T popLast() {
+        T data = tail.getData();
+        if(head == null) {
+            return null;
+        }
+        Node<T> secondLast = head;
+        while(secondLast.getNext()!= tail) {
+            secondLast = secondLast.getNext();
+        }
+        tail = secondLast;
+        tail.setNext(null);
+        return data;
+    }
+    public Node<T> search(T searchData){
+        Node<T> temp = head;
+        while(temp != null) {
+            if(temp.getData().equals(searchData))
+                return temp;
+            temp = temp.getNext();
+        }
+        return null;
+    }
+    public boolean searchAndInsert(T searchData, T insertData) {
+        Node<T> newMyNode = new Node(insertData);
+        Node<T> searchedMyNode = search(searchData);
+        if (searchedMyNode == null)
+            return false;
+        else {
+            newMyNode.setNext(searchedMyNode.getNext());
+            searchedMyNode.setNext(newMyNode);
+            return true;
+        }
+    }
 }
